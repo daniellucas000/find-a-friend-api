5f1c9d17ee576c8fc24ad613bc0d33e84011a920
danielllucas000 <danielluccas000@gmail.com>
2026-03-02 20:28:52 -0300

    feat: add CORS support and update environment variable handling

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import fastify from "fastify";
 import { appRoutes } from "./http/routes.js";
 import { ZodError } from "zod";
 import { env } from "./env/index.js";
+import cors from "@fastify/cors";
 
 export const app = fastify()
+
+app.register(cors, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+})
 
 app.register(appRoutes)
 
